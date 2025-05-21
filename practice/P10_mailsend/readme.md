7277d61113186e90f790b581474a57d6c144b5cb
Jace Kim <jacekim.theal@gmail.com>
2025-05-21 18:43:53 +0900

    실습 : 메일전송

---
+++ practice/P10_mailsend/readme.md
@@ -0,0 +1,10 @@
+# 간단한 메일보내기 구현
+
+- 구글에서 `npm mail` 이라고 검색
+<img src="./img/npm_nodemailer_01.png">
+
+- **Nodemailer** 패키지 설치
+```
+> npm i nodemailer
+```
+<img src="./img/npm_nodemailer_02.png">
---
+++ practice/hello.js
@@ -0,0 +1,9 @@
+console.log("Hello, Node.js!");
+
+let a = 10;
+let b = 20;
+let sum = a + b;
+console.log("The sum of " + a + " and " + b + " is: " + sum);
+console.log(`The sum of ${a} and ${b} is: ${sum}`);
+console.log(`The sum of ${a} and ${b} is: ${a + b}`);
+
